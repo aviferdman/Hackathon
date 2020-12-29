a34b38f6cc74e0fa09148f0b6491c3f6680ae180
aviferdman <ferdmaa@post.bgu.ac.il>
2020-12-29 15:45:44 +0200

    updated

--- client.py
+++ client.py
@@ -1,15 +1,22 @@
-import socket
+import msvcrt
 import os
-import time
+import socket
+import struct
 import sys
+import time
+
+from pynput import keyboard
 
 os.system("")
 
-UDP_PORT = 5005
-SERVER_TCP_PORT = 5006
+UDP_PORT = 13117
+TCP_PORT = 5006
 MESSAGE_LENGTH = 1024
 CLIENT_NICKNAME = "RAK BIBI\n"
 TIME_TO_PLAY = 10  # seconds
+BROADCAST_IP = ""
+sock = None
+end = time.time()
 
 
 # Group of Different functions for different styles
@@ -26,28 +33,43 @@ class style:
     RESET = '\033[0m'
 
 
-def main():
-    print(style.YELLOW + "Client started, listening for offer requests...")
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock.bind(("0.0.0.0", UDP_PORT))
-    data, address = sock.recvfrom(MESSAGE_LENGTH)
-    sock.close()
-    server_ip_address = data.decode()
-    print("Received offer from " + server_ip_address + " attempting to connect...")
+def pressed(pressed_key):
+    if time.time() < end:
+        try:
+            ch = str(pressed_key)[1]  # '{0}', char
+            sock.sendall(ch.encode())
+        except:
+            pass
 
+
+while True:
+
+    print(style.YELLOW + "Client started, listening for offer requests...")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((BROADCAST_IP, UDP_PORT))
+    data, address = sock.recvfrom(MESSAGE_LENGTH)
+    server_ip_address = address[0]
+    sock.close()
+    cookie, message_type, server_tcp_port = struct.unpack('LBH', data)
+    #   if cookie == 0xfeedbeef or message_type == 0x2:
+    print("Received offer from " + server_ip_address + " attempting to connect...")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((server_ip_address, SERVER_TCP_PORT))
+    sock.connect((server_ip_address, server_tcp_port))
     sock.sendall(CLIENT_NICKNAME.encode())
     print(style.GREEN + sock.recv(MESSAGE_LENGTH).decode())
-
+    listener = keyboard.Listener(on_press=pressed)
+    listener.start()
     end = time.time() + TIME_TO_PLAY
     while time.time() < end:  # wait for input
         pass
-    # TODO: understand how to send without enter at the end
-    sock.sendall(b"a")
-    print(sock.recv(MESSAGE_LENGTH).decode())
-
-
-if __name__ == "__main__":
-    main()
+    listener.stop()
+    feedback = ""
+    try:
+        feedback = sock.recv(MESSAGE_LENGTH).decode()
+    except:
+        feedback = "\nServer Disconnected"
+    print(feedback)
+    sock.close()
+    print("Server disconnected, listening for offer requests...")
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import random
 import socket
 import os
 import time
+import struct
 from threading import Thread
 
 os.system("")
@@ -21,41 +22,48 @@ class style:
     RESET = '\033[0m'
 
 
-UDP_IP = "255.255.255.255"
-UDP_PORT = 5005
+UDP_IP = '<broadcast>'
+UDP_PORT = 13117
 TCP_PORT = 5006
 MESSAGE_LENGTH = 1024
 TIME_TO_CONNECT = 10  # seconds
 TIME_TO_PLAY = 10  # seconds
+c_A = []
+c_B = []
+
+
+def make_bytes_message():
+    return struct.pack('LBH', 0xfeedbeef, 0x2, TCP_PORT)
 
 
 def send_broadcast():
     hostname = socket.gethostname()  # getting the hostname by socket.gethostname() method
     ip_address = socket.gethostbyname(hostname)  # getting the IP address using socket.gethostbyname() method
-    bytes_ip_address = ip_address.encode()
+    #   bytes_ip_address = ip_address.encode()
     message = "Server started, listening on IP address " + ip_address
     sock = socket.socket(socket.AF_INET,  # Internet
-                         socket.SOCK_DGRAM)  # UDP
+                         socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
     print(style.CYAN + message)
     t_end = time.time() + TIME_TO_CONNECT
     while time.time() < t_end:
-        sock.sendto(bytes_ip_address, (UDP_IP, UDP_PORT))
+        send_bytes = make_bytes_message()
+        sock.sendto(send_bytes, (UDP_IP, UDP_PORT))
         time.sleep(1)
     sock.close()
 
 
 def connect_clients(connection_client, sock):
     t_end = time.time() + TIME_TO_CONNECT
-    sock.settimeout(t_end - time.time())
+    sock.settimeout(t_end - time.time())  # accept is waiting for no mare than time left to connect
     while time.time() < t_end:
         try:
             connection, client_address = sock.accept()
             connection_client[connection] = client_address
-            print(style.GREEN+"New player connected")
+            print(style.GREEN + "New player connected")
         except:
-            print(style.RED+"Time is up for connecting new players")
+            print("Time is up for connecting new players")
     sock.close()
 
 
@@ -74,37 +82,52 @@ def generate_teams(connection_client, groupA, groupB):
 def get_group_names(group):
     names = ""
     for c in group.keys():
-        names += c.recv(MESSAGE_LENGTH).decode()
+        try:
+            names += c.recv(MESSAGE_LENGTH).decode()
+        except:
+            pass
     return names
 
 
 def generate_begin_message(namesA, namesB):
     begin_message = "Welcome to Keyboard Spamming Battle Royale.\nGroup 1:\n==\n"
     begin_message += namesA
-    begin_message += "Group 2:\n==\n"
+    begin_message += "\nGroup 2:\n==\n"
     begin_message += namesB
     return begin_message
 
 
+def send_message_to_group(message, group):
+    for c in group.keys():
+        try:
+            c.sendall(message)
+        except:
+            pass
+
+
 def send_message(begin_message, groupA, groupB):
     begin_message = begin_message.encode()
-    for c in groupA.keys():
-        c.sendall(begin_message)
-    for c in groupB.keys():
-        c.sendall(begin_message)
+    send_message_to_group(begin_message, groupA)
+    send_message_to_group(begin_message, groupB)
 
 
 def count_characters(group):
     count = 0
     for c in group.keys():
-        count += len(c.recv(MESSAGE_LENGTH).decode())
+        try:
+            c.settimeout(0)
+            message = c.recv(MESSAGE_LENGTH).decode()
+            count += len(message)
+        except:
+            pass
     return count
 
 
 def generate_end_message(groupA, groupB, namesA, namesB):
     countA = count_characters(groupA)
     countB = count_characters(groupB)
-    end_message = "Game over!\nGroup 1 typed in " + str(countA) + " characters. Group 2 typed in " + str(countB) + " characters.\n"
+    end_message = "Game over!\nGroup 1 typed in " + str(countA) + " characters. Group 2 typed in " + str(
+        countB) + " characters.\n"
     if countA > countB:
         end_message += "Group 1 wins!\nCongratulations to the winners:\n==\n"
         end_message += namesA
@@ -134,21 +157,23 @@ def start_game(connection_client):
 
 
 def main():
-    connection_client = {}
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # bind the socket to a public host, and a well-known port
-    sock.bind((socket.gethostname(), TCP_PORT))
-    # become a server socket
-    sock.listen(5)
-    broadcaster = Thread(target=send_broadcast, args=())
-    client_connector = Thread(target=connect_clients, args=(connection_client, sock,))
+    while True:
+        connection_client = {}
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # bind the socket to a public host, and a well-known port
+        sock.bind((socket.gethostname(), TCP_PORT))
+        # become a server socket
+        sock.listen(5)
+        broadcaster = Thread(target=send_broadcast, args=())
+        client_connector = Thread(target=connect_clients, args=(connection_client, sock,))
+        broadcaster.start()
+        client_connector.start()
+        broadcaster.join()
+        client_connector.join()
 
-    broadcaster.start()
-    client_connector.start()
-    broadcaster.join()
-    client_connector.join()
-
-    start_game(connection_client)
+        start_game(connection_client)
+        print(style.BLUE+"Game over, sending out offer requests...")
+        sock.close()
 
 
 if __name__ == "__main__":
